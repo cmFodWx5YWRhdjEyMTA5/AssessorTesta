a18dcc5f71884e27ffcabc217ba28fee3afe0c21
vipin10 <rrcoolguy234@gmail.com>
2019-05-22 16:49:47 +0530

    Assessor attendance done

---
+++ app/src/main/java/com/vipin/assessortesta/Batch_Student/Assessment_details.java
@@ -0,0 +1,15 @@
+package com.vipin.assessortesta.Batch_Student;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.vipin.assessortesta.R;
+
+public class Assessment_details extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assessment_details);
+    }
+}
